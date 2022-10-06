99e6f3d8a845f3db4f189ab20deba9904b53f06f
Sentfromevil <95447178+Sentfromevil@users.noreply.github.com>
2022-10-06 10:59:49 -0500

    si

--- index.php
+++ index.php
@@ -1,10 +1,18 @@
+<?php
+  header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+  header("Expires: Sat, 1 Jul 2000 05:00:00 GMT"); // Fecha en el pasado
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/CSS/login.css">
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <link rel="stylesheet" href="CSS/login.css">
     <title>login</title>
 </head>
 <body>
@@ -40,6 +48,6 @@
             </div>
         </div>
     </main>
-    <script src="/JS/script-login.js"></script>
+    <script src="JS/script-login.js"></script>
 </body>
 </html>
---
+++ php/conexion_be.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+$conexion = mysqli_connect("localhost","root","", "login_register_db");
+
+/*if($conexion){
+    echo 'Conectado exitoxamente a la base de datos';
+}else{
+    echo 'No se ha podido conectar a la base de datos';
+}*/
+
+
+
+
+?>
