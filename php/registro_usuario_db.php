97aecc47347da9efa3431ca3e8e7613194096b82
Sentfromevil <95447178+Sentfromevil@users.noreply.github.com>
2022-10-06 22:14:02 -0500

    s

--- php/registro_usuario_db.php
+++ php/registro_usuario_db.php
@@ -6,19 +6,55 @@
     $correo= $_POST['correo'];
     $usuario= $_POST['usuario'];
     $contrasena= $_POST['contrasena'];
+    $contrasena= hash('sha512', $contrasena);
 
     $query = "INSERT INTO usuarios(nombre,correo,usuario,contrasena)
               VALUES('$nombre','$correo','$usuario','$contrasena')";
+//verificacion de registro
+    $verificar_correo = mysqli_query($conexion,"SELECT * FROM usuarios WHERE correo='$correo' ");
+
+    if(mysqli_num_rows($verificar_correo) > 0){
+          echo'
+            <script>    
+                alert("Este Correo ya esta registrado");
+                window.location = "../index.php"
+            </script>
+
+          ';
+          exit();
+    }
+
+    $verificar_usuario = mysqli_query($conexion,"SELECT * FROM usuarios WHERE usuario='$usuario' ");
+
+    if(mysqli_num_rows($verificar_usuario) > 0){
+          echo'
+            <script>    
+                alert("Este usuario ya esta registrado");
+                window.location = "../index.php"
+            </script>
+
+          ';
+          exit();
+    }
 
     $ejecutar  = mysqli_query($conexion,$query);
 
     if($ejecutar){
         echo'
             <script>
-                alert("Usuario registrado exitosamentess");
+                alert("Usuario registrado exitosamentes");
+                window.location= "../index.php"
             </script>
         
         ';
+    }else{
+        echo'
+        <script>
+            alert("Intentalo nuevamente");
+            window.location= "../index.php"
+        </script>
+    
+    ';
     }
 
 
