f8a912a419c7111322ad466d5cb43e5a1c0ea584
Sentfromevil <95447178+Sentfromevil@users.noreply.github.com>
2022-10-08 06:13:19 -0500

    si

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,15 +1,3 @@
 <?php 
-
-
 $conexion = mysqli_connect("localhost","root","", "login_register_db");
-
-/*if($conexion){
-    echo 'Conectado exitoxamente a la base de datos';
-}else{
-    echo 'No se ha podido conectar a la base de datos';
-}*/
-
-
-
-
 ?>
